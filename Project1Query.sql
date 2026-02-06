c7b0dc1f5443788f307079289bd067677134f941
Michael Arellano <michael.arellano1@snhu.edu>
2026-02-05 23:54:21 -0800

    Update Project1Query.sql

--- Project1Query.sql
+++ Project1Query.sql
@@ -6,6 +6,11 @@ union
 select * from dbo.[2019rev]
 union
 select * from dbo.[2020rev])
+  
+select arrival_date_year, hotel, sum((stays_in_week_nights + stays_in_weekend_nights) * adr) as revenue,
+concat (round((sum(required_car_parking_spaces) / sum(stays_in_week_nights + stays_in_weekend_nights)) * 100, 2), '%') as parking_percentage
+from hotel_data
+group by arrival_date_year, hotel
 
 select * from hotel_data as hd
 left join dbo.market_segment as ms
@@ -13,10 +18,7 @@ on hd.market_segment = ms.market_segment
 left join dbo.meal_cost as mc
 on mc.meal = hd.meal
 
-select arrival_date_year, hotel, sum((stays_in_week_nights + stays_in_weekend_nights) * adr) as revenue,
-concat (round((sum(required_car_parking_spaces) / sum(stays_in_week_nights + stays_in_weekend_nights)) * 100, 2), '%') as parking_percentage
-from hotel_data
-group by arrival_date_year, hotel
+
 
 
 
