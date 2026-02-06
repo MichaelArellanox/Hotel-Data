729df9db349eb1126aff368d1dcbe8b035431ad4
Michael Arellano <michael.arellano1@snhu.edu>
2026-02-05 20:57:04 -0800

    Update Project1Query.sql

--- Project1Query.sql
+++ Project1Query.sql
@@ -13,3 +13,10 @@ on hd.market_segment = ms.market_segment
 left join dbo.meal_cost as mc
 on mc.meal = hd.meal
 
+select arrival_date_year, hotel, sum((stays_in_week_nights + stays_in_weekend_nights) * adr) as revenue,
+concat (round((sum(required_car_parking_spaces) / sum(stays_in_week_nights + stays_in_weekend_nights)) * 100, 2), '%') as parking_percentage
+from hotel_data
+group by arrival_date_year, hotel
+
+
+
